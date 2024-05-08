1ebb36f20549bf04c9ced7d32640265168bf8b2f
Abdi Kumela <abdikumela036@gmail.com>
2024-05-08 15:23:56 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = requrire("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from server side!!!" });
+});
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`App is running on port ${port}...`);
+});
