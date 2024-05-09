3b01a2197527f575e08b3f66a99a120d6bf1bcc2
Abdi Kumela <abdikumela036@gmail.com>
2024-05-09 15:55:12 +0300

    implementing GET request

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
+const fs = require("fs");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from server side!!!" });
+// app.get("/", (req, res) => {
+//   res.status(200).json({ message: "Hello from server side!!!" });
+// });
+
+const toursData = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: toursData.length,
+    data: {
+      tours: toursData,
+    },
+  });
 });
 
 const port = 8000;
