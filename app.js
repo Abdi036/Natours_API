377391a1d0a8e775e59103121a9f3637f4eac355
Abdi Kumela <abdikumela036@gmail.com>
2024-05-10 10:33:21 +0300

    implementing GET request with params

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const toursData = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// get request
 app.get("/api/v1/tours", (req, res) => {
   res.status(200).json({
     status: "success",
@@ -22,6 +23,7 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+// post request
 app.post("/api/v1/tours", (req, res) => {
   const newId = toursData[toursData.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
@@ -39,6 +41,18 @@ app.post("/api/v1/tours", (req, res) => {
       });
     }
   );
+});
+
+// get request with params
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const id = +req.params.id;
+  const tour = toursData.find((el) => el.id === id);
+
+  if (!tour) {
+    return res.status(404).json({ status: "fail", message: "INVALID ID" });
+  }
+  res.status(200).json({ status: "success", data: { tour } });
 });
 
 const port = 8000;
