455effd790f2fcb0c8509e5197410d1af8ac7178
Abdi Kumela <abdikumela036@gmail.com>
2024-05-10 10:10:12 +0300

    implementing Post request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 // app.get("/", (req, res) => {
 //   res.status(200).json({ message: "Hello from server side!!!" });
@@ -19,6 +20,25 @@ app.get("/api/v1/tours", (req, res) => {
       tours: toursData,
     },
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = toursData[toursData.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  toursData.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(toursData),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 8000;
