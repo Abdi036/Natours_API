14183e11dca830ba445342b3592f39cf00fe94c6
Abdi Kumela <abdikumela036@gmail.com>
2024-05-30 15:37:33 +0300

    creating schema

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: ".env" });
 
 const app = require("./app");
+// creating connection to database
 const DB = process.env.DATABASE.replace("<password>", process.env.DB_PASSWORD);
 
 mongoose
@@ -14,6 +15,20 @@ mongoose
   .catch((err) => {
     console.error("DB connection error:", err);
   });
+
+// schema for database
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+  },
+  rating: Number,
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
