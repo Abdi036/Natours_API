30004ae7c12195de6927183d491cd54f8f9a4a9c
Abdi Kumela <abdikumela036@gmail.com>
2024-07-04 00:00:34 +0300

    added some packages for security

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+
 const tourRouter = require("./routes/tourRoute");
 const userRouter = require("./routes/userRoute");
 
@@ -9,9 +13,17 @@ const app = express();
 // MIDDLEWARES
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(express.json({ limit: "10kb" }));
-app.use(express.static(`${__dirname}/public`));
 
+// Body parser Reading data from body to req,body
+app.use(express.json({ limit: "10kb" }));
+// Serving static Files
+app.use(express.static(`${__dirname}/public`));
+// Data sanitization against xss
+app.use(xss());
+// Data sanitization against NOSQL query injection
+app.use(mongoSanitize());
+// prevent parameter polution
+app.use(hpp());
 // Rate limiter middleware
 const limiter = rateLimit({
   max: 100, // Limit each IP to 100 requests per `window` (here, per hour)
